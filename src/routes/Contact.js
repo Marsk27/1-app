630b709d6cb89c9cfa0ea5ac5f91a7878ce657c2
Marsk27 <marskirova@gmail.com>
2023-04-28 20:32:33 +0200

    newfrontendbackend123456

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,7 +2,6 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import AboutImg from "../assets/2.jpg";
 import Footer from "../components/Footer";
-import ContactForm from "../components/ContactForm";
 import { useState} from "react";
 import axios from "axios";
 import "../components/ContactFormStyles.css";
@@ -13,7 +12,6 @@ function Contact() {
   const [subject, setSubject] = useState('')
   const [message, setMessage] = useState('')
   const [error, setError] = useState('')
-  const [selectValue] = useState('')
 
   const axiosPostData = async() => {
       const postData = {
